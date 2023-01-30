7b8b4863c8e7cf5d6948c1e32ea85084f2728807
LuckDev <lukedru@yahoo.com>
2023-01-29 18:08:47 -0800

    Initial Commit

---
+++ Pomo/PomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoApp.swift
+//  Pomo
+//
+//  Created by Luke Drushell on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct PomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
